3fdea6f663bb9afb61a1a907cdfe9c39af179e50
stefano bagli <stefano.bagli@gecosistema.it>
2024-03-15 00:06:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -215,14 +215,7 @@ def main():
             fig.update_traces(hoverinfo='x+y+z', showscale=True)
             st.plotly_chart(fig, use_container_width=True)            
 
-            # Plotting lower forecast_djf_median_sums with custom color scale
-            fig = px.imshow(forecast_djf_median_sums, 
-                            labels=dict(x="Longitude", y="Latitude", color="forecast_djf_median_sums"),
-                            x=forecast_djf_median_sums.lon,
-                            y=forecast_djf_median_sums.lat)
-            
-            fig.update_traces(hoverinfo='x+y+z', showscale=True)
-            st.plotly_chart(fig, use_container_width=True)            
+        
                         
 
             # Step 5: Interactive year selection and plotting
