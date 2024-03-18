60128267756562e28d2fcad76649cf164754bd3e
stefano bagli <stefano.bagli@gecosistema.it>
2024-03-18 13:10:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -237,7 +237,9 @@ def main():
                         x=below_normal_probability_forecast.lon,
                         y=below_normal_probability_forecast.lat,
                         color_continuous_scale=custom_color_scale)
-        
+        # Reverse the y-axis (latitude) to display negative values on the bottom
+        fig.update_yaxes(autorange="reversed")
+ 
         fig.update_traces(hoverinfo='x+y+z', showscale=True)
         st.plotly_chart(fig, use_container_width=True)            
         
@@ -246,8 +248,6 @@ def main():
                         labels=dict(x="Longitude", y="Latitude", color="lower_tercile"),
                         x=lower_tercile.lon,
                         y=lower_tercile.lat)
-        # Reverse the y-axis (latitude) to display negative values on the bottom
-        fig.update_yaxes(autorange="reversed")
         
         fig.update_traces(hoverinfo='x+y+z', showscale=True)
         st.plotly_chart(fig, use_container_width=True)            
